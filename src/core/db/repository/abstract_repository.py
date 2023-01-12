0c9517345ed2a71fc0b29a05c34ce22cce0c620d
Denis Shvetsov <den.shvetsov@yandex.ru>
2023-01-12 22:48:16 +0300

    CRUD updade

--- src/core/db/repository/abstract_repository.py
+++ src/core/db/repository/abstract_repository.py
@@ -28,7 +28,7 @@ class CRUDBase:
         session: AsyncSession,
     ):
         """create new record."""
-        # ! уточнить формат фходных данных для записи словарь или объект модели
+        # ! уточнить формат входных данных для записи словарь или объект модели
         # obj_in_data = obj_in.dict()
         # db_obj = self.model(**obj_in_data)
         db_obj = obj_in
@@ -37,26 +37,27 @@ class CRUDBase:
         await session.refresh(db_obj)
         return db_obj
 
-    # async def update(
-    #     self,
-    #     db_obj,
-    #     obj_in,
-    #     session: AsyncSession,
-    # ):
-    #     """Update record."""
-    #     # obj_data = jsonable_encoder(db_obj)
-    #     # update_data = obj_in.dict(exclude_unset=True)
-    #     # obj_data = db_obj.__mapper__
-    #     # print(obj_data)
-    #     # update_data = obj_in.__dict__.pop("_sa_instance_state", None)
-    #     # update_data = obj_in.__dict__
-    #     for field in obj_data:
-    #         if field in update_data:
-    #             setattr(db_obj, field, update_data[field])
-    #     session.add(db_obj)
-    #     await session.commit()
-    #     await session.refresh(db_obj)
-    #     return db_obj
+    async def update(
+        self,
+        db_obj,
+        obj_in,
+        session: AsyncSession,
+    ):
+        """Update record."""
+        db_obj_keys = [column.key for column in db_obj.__table__.columns]
+        update_obj = obj_in.__dict__
+        update_obj.pop("_sa_instance_state", None)
+        obj_in_keys = update_obj.keys()
+        for db_key in db_obj_keys:
+            if db_key != "id" and db_key in obj_in_keys:
+                db_value = getattr(db_obj, db_key)
+                update_value = getattr(obj_in, db_key)
+                if update_value != db_value:
+                    setattr(db_obj, db_key, update_value)
+        session.add(db_obj)
+        await session.commit()
+        await session.refresh(db_obj)
+        return db_obj
 
     async def remove(
         self,
