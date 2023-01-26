9357c1c54440da6359b5b7483b10d134e9fe116f
Gorynichev.AA <px2005@yandex.ru>
2023-01-26 16:53:31 +0300

    admin_console_flask v2.01

--- admin/run.py
+++ admin/run.py
@@ -3,30 +3,30 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
 
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+load_dotenv()
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
 from src.core.db.model import (User, Member, Request,
                                Shift, Report, Task,
                                Volunteer, Pollution, Assistance_disabled)
-from src.core.db.db import database_url
-
 
 app = Flask(__name__)
 app.config['FLASK_ENV'] = 'development'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = database_url
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f"postgresql://{os.getenv('POSTGRES_USER')}:"
+    f"{os.getenv('POSTGRES_PASSWORD')}@"
+    f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('POSTGRES_DB')}")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 
-
-# class URLMap(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     original = db.Column(db.String(256), nullable=False)
-#     short = db.Column(db.String(16), unique=True, nullable=False)
-
-
 admin = Admin(app, name='bot_delo_zhivet', template_mode='bootstrap3')
-# admin.add_view(ModelView(URLMap, db.session, name='URLMap'))
 
 admin.add_view(ModelView(User, db.session, name='User'))
 admin.add_view(ModelView(Member, db.session, name='Member'))
@@ -40,7 +40,5 @@ admin.add_view(
     ModelView(Assistance_disabled, db.session, name='Assistance_disabled')
 )
 
-
 if __name__ == '__main__':
-    # db.create_all()
     app.run(debug=True)
