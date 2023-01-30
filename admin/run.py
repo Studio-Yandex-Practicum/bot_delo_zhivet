8cb94957eb035c13f2968020ec257a7a5ec37f45
Gorynichev.AA <px2005@yandex.ru>
2023-01-30 09:55:19 +0300

    Создание шаблонов и моделей для авторизации приложения админ панель

--- admin/run.py
+++ admin/run.py
@@ -1,7 +1,11 @@
-from flask import Flask
+import flask_admin
+from flask import abort, Flask, render_template, redirect, url_for, request
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import (Security, SQLAlchemyUserDatastore,
+                            UserMixin, RoleMixin, login_required, current_user
+                            )
 
 from dotenv import load_dotenv
 import os
@@ -13,6 +17,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['FLASK_ENV'] = 'development'
+app.config.from_pyfile('config.py')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
     f"postgresql://{os.getenv('POSTGRES_USER')}:"
@@ -22,7 +27,79 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 
-admin = Admin(app, name='bot_delo_zhivet', template_mode='bootstrap3')
+# Define models
+roles_users = db.Table(
+    'roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+)
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+    def __str__(self):
+        return self.name
+
+
+class User1(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+    def __str__(self):
+        return self.email
+
+
+# Setup Flask-Security
+# user_datastore = SQLAlchemyUserDatastore(db, User1, Role)
+# security = Security(app, user_datastore)
+
+
+# Create customized model view class
+class MyModelView(ModelView):
+    def is_accessible(self):
+        return (current_user.is_active and
+                current_user.is_authenticated and
+                current_user.has_role('superuser')
+                )
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for('security.login', next=request.url))
+
+
+# Flask views
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Create admin
+admin = flask_admin.Admin(
+    app,
+    'Example: Auth',
+    base_template='my_master.html',
+    template_mode='bootstrap4',
+)
+
+# admin = Admin(app, name='bot_delo_zhivet', template_mode='bootstrap3')
 
 admin.add_view(ModelView(User, db.session, name='User'))
 admin.add_view(ModelView(Volunteer, db.session, name='Volunteer'))
