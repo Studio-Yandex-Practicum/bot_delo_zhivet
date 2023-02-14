798430a97434aa024933e62cf754b32da2cdca27
zakongyka <zakongyka@yandex.ru>
2023-02-14 17:17:27 +0300

    isort

--- src/core/db/repository/user_repository.py
+++ src/core/db/repository/user_repository.py
@@ -1,3 +1,7 @@
+import json
+
+import backoff
+import requests as requests
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -8,7 +12,21 @@ from src.core.db.repository.abstract_repository import CRUDBase
 logger = get_logger()
 
 
+def backoff_hdlr(details):
+    # print("Backing off {wait:0.1f} seconds after {tries} tries ")
+    logger.error("Error detected, %s attempt to connect to base", details.get("tries"))
+
+
+# def save_dict_to_json(path, data):
+#     logger.info('Saving new state file to %s', path)
+#     with open(path, 'w') as fil:
+#         fil.write(json.dumps(data))
+
+
 class UserCRUD(CRUDBase):
+    @backoff.on_exception(
+        backoff.expo, on_backoff=backoff_hdlr, on_giveup=print("ALERT"), max_tries=10, max_time=20, exception=Exception
+    )
     async def get_user_id_by_telegram_id(
         self,
         telegram_id: int,
