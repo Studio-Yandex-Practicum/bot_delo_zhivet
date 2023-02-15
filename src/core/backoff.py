eb8a9e48a23acf964a50f52510de291642a2d38c
zakongyka <zakongyka@yandex.ru>
2023-02-15 23:04:20 +0300

    v_0.1 | with out logger

--- src/core/backoff.py
+++ src/core/backoff.py
@@ -2,15 +2,18 @@ import backoff
 from bot.handlers.loggers import get_logger
 
 logger = get_logger()
+
 """
-Base_method - арифметическая прогрессия
-RandomJitter_method - арифметическая прогрессия с добавлением случайного числа милисекунд
-FullJitter_method - алгоритм «Full Jitter», определенный в публикации «Экспоненциальное отставание и джиттер» в блоге об архитектуре AWS.
+Base_method - арифметическая прогрессия 
+RandomJitter_method - арифметическая прогрессия
+                    с добавлением случайного числа милисекунд
+FullJitter_method - алгоритм «Full Jitter», определенный в публикации
+        «Экспоненциальное отставание и джиттер» в блоге об архитектуре AWS.
 """
 JITTER = {
-        "Base_method": None,
-        "RandomJitter_method": backoff.random_jitter,
-        "FullJitter_method": backoff.full_jitter
+    "Base_method": None,
+    "RandomJitter_method": backoff.random_jitter,
+    "FullJitter_method": backoff.full_jitter
 }
 
 
@@ -18,24 +21,14 @@ def backoff_hdlr(details):
     tries = details.get("tries")
     wait = float('{:.3f}'.format(details.get("wait")))
     elapsed = float('{:.3f}'.format(details.get("elapsed")))
-    # logger.error("Error detected, %s attempt to connect to base", details.get("tries"))
-    # logger.error(f'Error detected:\n'
-    #              f'{tries} attempt to connect to base\n'
-    #              f'Wait time {wait}\n'
-    #              f'Total waiting time {elapsed} seconds'
+    # logger.error(msg=(f'Error detected:\n'
+    #                   f'{tries} attempt to connect to base\n'
+    #                   f'Wait time {wait}\n'
+    #                   f'Total waiting time {elapsed} seconds'
+    #                   )
     #              )
     print(f'Error detected:\n'
           f'Connection attempt number: {tries}\n'
           f'Wait time: {wait}\n'
           f'Total waiting time: {elapsed} seconds'
           )
-
-
-'''  
-@backoff.on_exception(backoff.expo,
-                      exception=Exception,
-                      on_backoff=backoff_hdlr,
-                      max_tries=10,
-                      max_time=60,
-                      )
-'''
