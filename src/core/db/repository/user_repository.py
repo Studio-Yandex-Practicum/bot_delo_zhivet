1e25f8cf043fe539ac171572ef6af2986f87b750
zakongyka <zakongyka@yandex.ru>
2023-02-15 23:08:52 +0300

    v_0.1 | PEP8

--- src/core/db/repository/user_repository.py
+++ src/core/db/repository/user_repository.py
@@ -1,25 +1,28 @@
+import backoff
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-import backoff
+
 from bot.handlers.loggers import get_logger
+from src.core.backoff import JITTER, backoff_hdlr
 from src.core.db.model import User
 from src.core.db.repository.abstract_repository import CRUDBase
-from src.core.backoff import backoff_hdlr, JITTER
+
 logger = get_logger()
 
 
 class UserCRUD(CRUDBase):
-    @backoff.on_exception(backoff.expo,
-                          exception=Exception,
-                          on_backoff=backoff_hdlr,
-                          max_tries=12,
-                          max_time=120,
-                          jitter=JITTER["RandomJitter_method"],
-                          )
+    @backoff.on_exception(
+        backoff.expo,
+        exception=Exception,
+        on_backoff=backoff_hdlr,
+        max_tries=12,
+        max_time=120,
+        jitter=JITTER["RandomJitter_method"],
+    )
     async def get_user_id_by_telegram_id(
-            self,
-            telegram_id: int,
-            session: AsyncSession,
+        self,
+        telegram_id: int,
+        session: AsyncSession,
     ):
         user_id = await session.execute(select(User.id).where(User.telegram_id == telegram_id))
         return user_id.scalars().first()
