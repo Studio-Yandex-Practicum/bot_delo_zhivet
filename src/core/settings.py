f6ff2b086ee66e25d1698bef0dda494c8cca3347
dimma <121demon@mail.ru>
2023-02-16 15:09:55 +0300

    webhook mode added

--- src/core/settings.py
+++ src/core/settings.py
@@ -6,6 +6,10 @@ load_dotenv()
 
 class Settings(BaseSettings):
     telegram_bot_token: str = None
+    webhook_domain: str = None
+    webhook_port: int = None
+    webhook_path: str = None
+    host: str = None
 
     class Config:
         env_file = ".env"
