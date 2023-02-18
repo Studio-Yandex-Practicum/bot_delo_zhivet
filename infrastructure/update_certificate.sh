0fd2c32d568232b777af417f453ee8143098e121
Vladislav Vasileknko <vasilekx8@yandex.ru>
2023-02-18 23:09:21 +0300

    Update the testing environment

--- infrastructure/update_certificate.sh
+++ infrastructure/update_certificate.sh
@@ -36,5 +36,7 @@ else
         echo "Successfully received certificate."
         docker-compose exec nginx nginx -s reload
         echo "Reloaded the nginx configuration."
+        export NEED_REBOOT_CONF_TRUE="FALSE"
+        echo "Set NEED_REBOOT_CONF=FALSE"
     fi
 fi
