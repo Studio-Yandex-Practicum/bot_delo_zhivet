89f23ccdc964b41e20daff83bbc29bee115b42ff
unknown <loshchilov.aleksandr@gmail.com>
2023-02-23 16:14:47 +0300

    Add model views to admin panel. Add translation tool for field name. Add pagination by default. Add search by tg user name. Disable create and delete records

--- admin/database.py
+++ admin/database.py
@@ -19,13 +19,11 @@ def create_roles_and_superuser():
         staff = Staff(
             login=os.getenv("SUPER_USER_LOGIN"),
             email=os.getenv("SUPER_USER_EMAIL"),
-            #password=os.getenv("SUPER_USER_PASSWORD"),
             active=True,
         )
         staff.set_password(os.getenv("SUPER_USER_PASSWORD"))
         role = Role(name="superuser", description="superuser")
         role_admin = Role(name="admin", description="admin")
-        
         staff.roles.append(role)
         db_session.add(staff)
         db_session.add(role)
