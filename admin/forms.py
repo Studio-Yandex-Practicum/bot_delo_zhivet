c0c957fe0b6e1bca3ab30b7f191c17fe1ce32b25
unknown <loshchilov.aleksandr@gmail.com>
2023-02-23 17:28:35 +0300

    Add possibility for admin to see user tab

---
+++ admin/forms.py
@@ -0,0 +1,44 @@
+from werkzeug.security import check_password_hash
+from wtforms import fields, form, validators
+
+from src.core.db.model import Staff
+
+from .database import db
+
+
+class LoginForm(form.Form):
+    """Форма входа в админку"""
+
+    login = fields.StringField(validators=[validators.InputRequired()])
+    password = fields.PasswordField(validators=[validators.InputRequired()])
+
+    def validate_login(self, field):
+        user = self.get_user()
+
+        if user is None:
+            raise validators.ValidationError("Неверный пользователь")
+
+        if not check_password_hash(user.password, self.password.data):
+            raise validators.ValidationError("Неверный пароль")
+
+    def get_user(self):
+        return db.session.query(Staff).filter_by(login=self.login.data).first()
+
+
+class RegistrationForm(form.Form):
+    """Форма регистрации"""
+
+    login = fields.StringField("Учетная запись", validators=[validators.InputRequired()])
+    email = fields.StringField("Электронная почта", validators=[validators.DataRequired(), validators.Email()])
+    password = fields.PasswordField("Пароль", validators=[validators.InputRequired(), validators.Length(min=8)])
+    password2 = fields.PasswordField(
+        "Повторите пароль", validators=[validators.DataRequired(), validators.EqualTo("password")]
+    )
+
+    def validate_login(self, field):
+        if db.session.query(Staff).filter_by(login=self.login.data).count() > 0:
+            raise validators.ValidationError("Пользователь с такой учетной записью уже существует")
+
+    def validate_email(self, field):
+        if db.session.query(Staff).filter_by(email=self.email.data).count() > 0:
+            raise validators.ValidationError("Пользователь с таким адресом электронной почты существует")
