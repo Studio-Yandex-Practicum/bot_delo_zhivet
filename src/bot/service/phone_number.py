5e5453049c1a17ae3ed4c23577af752c14dba226
Андрей Изотов <andrej.izotoff@yandex.ru>
2023-03-07 12:51:25 +0300

    вернул проверку flake8

--- src/bot/service/phone_number.py
+++ src/bot/service/phone_number.py
@@ -3,6 +3,6 @@ import re
 
 def phone_number_validate(user_input):
     validate_phone_number_pattern = (
-        "^\+?(?!(?:.*-){3})(?!.*--)(?=[^()]*\([^()]+\)[^()]*$|[^()]*$)(?!.*-.*[()])(?:[()-]*\d){11}[()-]*$"
+        "^\+?(?!(?:.*-){3})(?!.*--)(?=[^()]*\([^()]+\)[^()]*$|[^()]*$)(?!.*-.*[()])(?:[()-]*\d){11}[()-]*$"  # noqa
     )
     return re.match(validate_phone_number_pattern, user_input)
