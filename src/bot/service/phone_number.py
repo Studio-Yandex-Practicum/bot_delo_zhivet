1d56538d4087722894ab75bf422398f7154b2730
Андрей Изотов <andrej.izotoff@yandex.ru>
2023-03-10 11:23:00 +0300

    добавил проверку первой цифры

--- src/bot/service/phone_number.py
+++ src/bot/service/phone_number.py
@@ -1,7 +1,7 @@
 def phone_number_validate(user_input):
-    numbers = list(filter(str.isdigit, user_input))[1:]
-    if len(numbers) == 10:
-        return "+7{}{}{}{}{}{}{}{}{}{}".format(*numbers)
+    numbers = list(filter(str.isdigit, user_input))
+    if len(numbers) == 11 and (numbers[0] == "7" or numbers[0] == "8"):
+        return "+7{}{}{}{}{}{}{}{}{}{}".format(*numbers[1:])
     return None
 
 
