5d06a1d22a595ca3f0229b49f1d825d222c7e09d
unknown <dimonveli@yahoo.com>
2023-06-02 17:32:14 +0300

    add add_new_volunteer_to_issue

--- src/bot/service/get_issues_with_statuses.py
+++ src/bot/service/get_issues_with_statuses.py
@@ -1,9 +1,13 @@
+from datetime import date
 import os
 
 from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import AsyncSession
 from yandex_tracker_client import TrackerClient
 
-from bot.handlers.state_constants import POLLUTION, SOCIAL
+from src.bot.handlers.state_constants import POLLUTION, SOCIAL
+from src.core.db.model import Volunteer
+from src.core.db.repository.volunteer_repository import crud_volunteer
 
 load_dotenv()
 
@@ -20,3 +24,24 @@ def get_issues_with_statuses(
         filter=dict(queue=queues, status=statuses)
     )
     return issues
+
+
+async def add_new_volunteer_to_issue(
+    volunteer: Volunteer,
+    session: AsyncSession
+):
+    format = "%d/%m/%Y"
+    open_issues = get_issues_with_statuses()
+    issues_in_radius = await crud_volunteer.get_issues_in_radius(
+        volunteer.longitude, volunteer.latitude, volunteer.radius,
+        session
+    )
+    for issue in open_issues:
+        if issue.key in issues_in_radius:
+            issue = client.issues[issue.key]
+            description = issue.description
+            description = '\n' + date.today().strftime(
+               format
+            ) + " Найден новый волонтер " 
+            description = 'c машиной' if volunteer.has_car else 'без машины'
+            issue.update(description=description)
