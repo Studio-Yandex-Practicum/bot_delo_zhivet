b6ad03179d0d08833b7315707ad20e7d392bde33
Masha Yovchik <mashkay@gmail.com>
2023-07-11 19:24:07 +0200

    +logger in status transition

--- src/bot/service/holiday.py
+++ src/bot/service/holiday.py
@@ -1,3 +1,4 @@
+from bot.handlers.loggers import logger
 from src.api.constants import VOLUNTEER_STATUS
 from src.core.db.model import Volunteer
 
@@ -5,14 +6,21 @@ from src.core.db.model import Volunteer
 def chek_and_update_holiday_status(volunteer: Volunteer, issue):
     if volunteer.is_banned:
         return
+    current_status = issue.status.key
     transitions = issue.transitions.get_all()
     transitions_ids = [transition.id for transition in transitions]
     if get_volunteer_is_in_vocation(volunteer):
         if VOLUNTEER_STATUS.otpusk.value in transitions_ids:
             return issue.transitions[VOLUNTEER_STATUS.otpusk.value].execute()
+        if current_status == VOLUNTEER_STATUS.otpusk.value:
+            return
+        logger.error(f"Status {VOLUNTEER_STATUS.otpusk.value} not found in VOLUNTEER statuses")
         return
     if VOLUNTEER_STATUS.open.value in transitions_ids:
         return issue.transitions[VOLUNTEER_STATUS.open.value].execute()
+    if current_status == VOLUNTEER_STATUS.open.value:
+        return
+    logger.error(f"Status {VOLUNTEER_STATUS.open.value} not found in VOLUNTEER statuses")
 
 
 def get_volunteer_is_in_vocation(volunteer: Volunteer) -> bool:
